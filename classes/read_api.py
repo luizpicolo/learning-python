ce0a1303cef99144ace51d67485ac63c37a5c588
Luiz Picolo <luizpicolo@gmail.com>
2021-02-16 21:08:14 -0400

    Fixed conflits

--- classes/read_api.py
+++ classes/read_api.py
@@ -5,10 +5,7 @@ class ReadAPI:
     def __init__(self, user):
         self.user = user
 
-<<<<<<< HEAD
     # Returns a json object according to the data_api parameter 
-=======
->>>>>>> 8076e762ee01473bf91baa2bcaf02abf1c93e567
     def request_api(self, data_api = ''):
         response = requests.get(
             f'https://api.github.com/users/{self.user}{self.__format_uri(data_api)}'
@@ -19,10 +16,7 @@ class ReadAPI:
         else:
             return response.status_code
 
-<<<<<<< HEAD
     # Return array with all repositories
-=======
->>>>>>> 8076e762ee01473bf91baa2bcaf02abf1c93e567
     def repositories(self):
         repos = []
         
@@ -35,18 +29,12 @@ class ReadAPI:
 
         return repos
     
-<<<<<<< HEAD
     # Returns an array with all information about the user
-=======
->>>>>>> 8076e762ee01473bf91baa2bcaf02abf1c93e567
     def info_user(self):
         data_api = self.request_api()
         return data_api
 
-<<<<<<< HEAD
     # Private Method
-=======
->>>>>>> 8076e762ee01473bf91baa2bcaf02abf1c93e567
     def __format_uri(self, param = ''):
         return f'/{param}' if param != '' else param
 
